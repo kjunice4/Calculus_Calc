7ed4e78e81d880477037ff5c22c710c26cbc5bef
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-01-12 21:52:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,11 +29,20 @@ Builder.load_string("""
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
             height: 200
-            text: "KSquared Calculus Calculator"
+            text: "Calculus Calculator"
+            on_release:
+                app.root.current = "Menu"
+                root.manager.transition.direction = "left"         
+        Button:
+            font_size: 60
+            background_color: 0, 0 , 0 , 1
+            size_hint_y: None
+            height: 200
+            text: "KSquared-math,LLC ©"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
-
+                
 """)
 
 #Menu
